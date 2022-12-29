3e3f9244e72791dbb1f112a3f764377a8b602734
git-rp <patilrahul000@gmail.com>
2022-12-29 10:44:52 +0530

    object litral and annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -5,3 +5,17 @@ let nullvalue: null = null;
 
 // Build in object
 let now: Date = new Date();
+
+//Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 1, 2];
+
+//Classes
+class Car {}
+let car: Car = new Car();
+
+//Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
