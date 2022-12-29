aed8c1646f1b818bdb85a2264746314f0fc0aab2
git-rp <patilrahul000@gmail.com>
2022-12-29 10:51:02 +0530

    type inference

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -2,10 +2,11 @@ let apple: number = 5;
 let speed: string = 'Fast';
 let hasname: boolean = true;
 let nullvalue: null = null;
+let demo = 'test';
 
 // Build in object
 let now: Date = new Date();
-
+let now1 = new Date();
 //Array
 let colors: string[] = ['red', 'green', 'blue'];
 let myNumbers: number[] = [1, 1, 2];
@@ -18,4 +19,13 @@ let car: Car = new Car();
 let point: { x: number; y: number } = {
   x: 10,
   y: 20,
+};
+
+//Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+const logNumber1 = (i: number) => {
+  console.log(i);
 };
